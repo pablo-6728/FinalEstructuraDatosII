5e70338291738c2f22f4321ed6ef59aa63d5e21d
Pablo Paladino <pameljuanpi@gmail.com>
2020-06-29 12:04:47 -0500

    Encolar y desencolar

--- Proyecto Final Estructuras I/Proyecto Final Estructuras I/main.cpp
+++ Proyecto Final Estructuras I/Proyecto Final Estructuras I/main.cpp
@@ -51,6 +51,61 @@ cola :: ~cola(){  //deconstructor que libera la memoria
     delete arr;
 }
 
+//funcion para quitar un elemetno de la cola
+void cola::dequeue(){
+    
+    //revisar para subdesbordamiento
+    if (vacia()) {
+        cout<<"\nSubdesbordamiento\nCierre del Programa\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    cout<<"\nQuitando "<< arr[frente]<<"\n";
+    frente = (frente + 1)%capacidad;
+    count--;
+
+}
+
+//funcion para agregar un elemento en la cola
+void cola::enqueue(int item){
+    
+    //verificar desbordamiento
+    if (llena()) {
+        cout<<"Desbordamiento\nCierre del Programa\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    cout<<"Insertando "<< item <<"\n";
+    
+    rear = (rear + 1)%capacidad;
+    arr[rear] = item;
+    count++;
+    
+}
+
+//funcion que muestra el ultimo elemento de la cola
+int cola::peek(){
+    
+    if (vacia()){
+            cout << "\nSobredesbordamiento\nCierre del Programa\n";
+            exit(EXIT_FAILURE);
+        }
+        return arr[frente];
+}
+
+//funcion que regresa el tamano de la cola
+int cola::size(){
+    return count;
+}
+
+//funcion que revisa si la cola esta vacia
+bool cola::vacia(){
+    return (size()==0);
+}
+
+bool cola::llena(){
+    return (size()== capacidad);
+}
 
 
 void hoja_presentacion(){
@@ -58,6 +113,32 @@ void hoja_presentacion(){
 }
 
 void operaciones_cola(){
+    int menu;
+    int input;
+    cola q(5);
+    
+    cout<<"\nLa cola solo acepta 5 elementos\n";
+   
+    do {
+        cout<<"\n\nOperaciones con Cola\n[1]Insertar en la cola\n[2]Eliminar de la cola\n[3]Mostrar la Pila\n[4]Volver al Menu Principal\n";
+        cin>>menu;
+        
+        if (menu == 1) {        //encolar
+            cout<< "\nIntoduzca el elemento que desea encolar\n";
+            cin>>input;
+            q.enqueue(input);
+            
+        }else{
+            
+            if (menu == 2) {        //desencolar
+                cout<< "\nDesencolando...\n";
+                q.dequeue();
+            }
+            
+        }
+        
+        
+    } while (menu!=4);
     
 }
 
