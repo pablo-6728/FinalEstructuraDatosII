d485a33c95605d39faed1a73767bd991b1fcdb37
Pablo Paladino <pameljuanpi@gmail.com>
2020-07-06 12:45:02 -0500

    Reporte de errores en el codigo

--- Proyecto Final Estructuras I/Proyecto Final Estructuras I/main.cpp
+++ Proyecto Final Estructuras I/Proyecto Final Estructuras I/main.cpp
@@ -15,9 +15,6 @@
 
 using namespace std;
 
-
-int menu;
-
 void hoja_presentacion(){
     std::cout<< "\n\nUniversidad Tecnologica de Panama\nCurso:Estructura de Datos I\nAlumnos: Pablo Paladino, Michael Xia\n";
 }
@@ -91,7 +88,18 @@ void operaciones_pila(){        //operaciones con las pilas
    
     do {
         cout<<"\n\nOperaciones con Pila\n[1]Insertar en la pila\n[2]Eliminar de la pila\n[3]Mostrar la Pila\n[4]Volver al Menu Principal\n";
-        cin>>menu;
+        std::cin>>menu;
+        
+        while (!std::cin.good()) {
+            
+            //limpiar el cin
+            std::cin.clear();
+            std::cin.ignore(INT_MAX,'\n');
+            
+            //pedir input again
+            std::cout<<"\nError: solo se aceptan numeros, intente de nuevo\n";
+            std::cin>>menu;
+        }
         
         if (menu == 1) {
             cout<< "\nIntoduzca el elemento que desea en pila\n";
@@ -101,8 +109,7 @@ void operaciones_pila(){        //operaciones con las pilas
         }else if (menu == 2) {
             
             if (val==NULL) {
-                cout<<"\nLa Pila esta vacia\nCierre del Programa\n";
-                exit(EXIT_FAILURE);
+                cout<<"\nLa Pila esta vacia\n";
             }
             
             else{
@@ -123,14 +130,27 @@ void operaciones_pila(){        //operaciones con las pilas
         
     }while (menu!=4);}
 
-void operaciones_cola(){
-    int input, menu;
+int operaciones_cola(){
+    int menu, input;
     Nodo* val = NULL;
     Nodo* d;
     
     do {
         cout<<"\n\nOperaciones con Cola\n[1]Insertar en la cola\n[2]Eliminar de la cola\n[3]Mostrar la Cola\n[4]Volver al Menu Principal\n";
-        cin>>menu;
+        
+        std::cin>>menu;
+        
+        while (!std::cin.good()) {
+            
+            //limpiar el cin
+            std::cin.clear();
+            std::cin.ignore(INT_MAX,'\n');
+            
+            //pedir input again
+            std::cout<<"\nError: solo se aceptan numeros, intente de nuevo\n";
+            std::cin>>menu;
+        }
+        
         
         if (menu == 1) { //encolar
             cout<<"Introduzca el elemento que desea agregar a la cola: \n";
@@ -144,8 +164,7 @@ void operaciones_cola(){
             if (menu == 2) {        //desencolar
 
                 if (val==NULL) {
-                    cout<<"\nLa Pila esta vacia\nCierre del Programa\n";
-                    exit(EXIT_FAILURE);
+                    cout<<"\nERROR: La Cola esta vacia\n";
                 }
                            
                 else{
@@ -167,31 +186,57 @@ void operaciones_cola(){
         }
             
     }while (menu!=4);
+    return 0;
+}
+
+int input(){
+    int menu = 0;
     
+    std::cin>>menu;
+    
+    //revisar error en menu
+    while (!std::cin.good()) {
+        
+        //limpiar el cin
+        std::cin.clear();
+        std::cin.ignore(INT_MAX,'\n');
+        
+        //pedir input again
+        std::cout<<"\nError: solo se aceptan numeros, intente de nuevo\n";
+        std::cin>>menu;
+    }
+    
+    return menu;
 }
 
 
-int main()
-{
-    do{
-        std::cout<< "Operaciones con Estructuras de datos lineales\n\n[1] Hoja de Presentacion\n[2]Operaciones con Pila\n[3]Operaciones con Cola\n[4]Salir\n";
+int main(){
+    
+    int menu;
+    
+        do{
+            std::cout<< "Operaciones con Estructuras de datos lineales\n\n[1] Hoja de Presentacion\n[2]Operaciones con Pila\n[3]Operaciones con Cola\n[4]Salir\n";
         
-        std::cin >> menu;
-        
-        if (menu == 1) {
-            hoja_presentacion();
-        }else{
+            menu = input();
+
+            if (menu == 1) {
+                hoja_presentacion();
+            }else{
             
-            if (menu == 2) {
-                operaciones_pila();
-            }
+                if (menu == 2) {
+                    operaciones_pila();
+                }
             
-            if (menu == 3) {
-                operaciones_cola();
+                if (menu == 3) {
+                   operaciones_cola();
+                }
+                if(menu > 4){
+                    cout<<"\nERROR: esa opcion no exite\n";
+                    menu = 0;
+                }
             }
-        }
         
-    }while (menu!=4);
-        
+        }while (menu!= 4);
+
 }
 
